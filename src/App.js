f9fd7714e761b54e633f3f2c7408b6e90692419e
Shivaditya Shivganesh <shivaditya.shivganesh2019@vitstudent.ac.in>
2022-06-03 11:13:14 +0530

    Base Setup Complete

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/*
+Home Page
+*/
+
+
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+
+
+const App = () => {
+    return (
+        <View>
+            <Text>Sankhya Home</Text>
+        </View>
+    )}
+
+export default App;
